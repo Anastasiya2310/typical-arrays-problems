6efa7e91d22fad756f48cd5cb097e4d3b2870f1d
Anastasiya Kharitonova <kovalskaya.anastasiya@gmail.com>
2020-02-20 22:48:14 +0300

    typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if(arr === undefined || arr.length === 0) {
+    return 0;
+  }
+  let minValue = arr[0];
+  for(let i = 0; i < arr.length; i++) 
+  {
+    if(minValue > arr[i]) {
+      minValue = arr[i];
+    }
+  }
+  return minValue;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if(arr === undefined || arr.length === 0) {
+    return 0;
+  }
+  let maxValue = arr[0];
+  for(let i = 0; i < arr.length; i++) 
+  { 
+    if(maxValue < arr[i]) {
+      maxValue = arr[i];
+    }
+  } 
+  return maxValue;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if(arr === undefined || arr.length === 0) {
+    return 0;
+  }
+  let avgValue = 0;
+  for(let i = 0; i < arr.length; i++) 
+  {
+    avgValue += arr[i] 
+  }
+  return avgValue/arr.length;
 }
